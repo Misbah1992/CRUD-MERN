22c913f4d21eb0d18ed05bbb5b24ad014b46fa4e
Misbah1992 <misbah199219@gmail.com>
2025-08-05 18:51:24 +0500

    api's intergrated on frontend

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useState } from 'react';
 import axios from "axios";
+import { useEffect } from 'react';
+import { MdDelete } from "react-icons/md";
+import { FaPen } from "react-icons/fa";
+
 
 function Home() {
     const [bookForm, setBookForm] = useState({
@@ -11,6 +15,9 @@ function Home() {
         PublishDate: ""
     })
 
+    const [bookList, setBookList] = useState([])
+    const [id, setId] = useState(null)
+
     const handleFormChange = (e) => {
         const { name, value } = e.target;
         setBookForm((prev) => ({
@@ -18,8 +25,39 @@ function Home() {
             [name]: value
         }))
     }
-    const handleSubmit = async () => {
+    
+
+    const getAllBookList = async () => {
         try {
+            const { data } = await axios.get("http://localhost:8000/book/booklists");
+            setBookList(data?.BookList)
+            console.log("booklist", data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleSubmit = async () => {
+        console.log(id)
+        if (id) {
+
+
+            const { data } = await axios.put("http://localhost:8000/book/updatebook", { ...bookForm, _id: id });
+            
+            if (data?.Success) {
+                setBookForm({
+                    BookName: "",
+                    BookTitle: "",
+                    Author: "",
+                    SellingPrice: "",
+                    PublishDate: "",
+                });
+                
+                setId(null);
+                getAllBookList();
+            }
+        }
+        else {
             const { data } = await axios.post("http://localhost:8000/book/addbook", bookForm);
             if (data?.Success) {
                 setBookForm({
@@ -30,11 +68,38 @@ function Home() {
                     PublishDate: ""
                 })
             }
-            console.log("data", data)
-        } catch (err) {
-            console.log(err)
+              
+        }
+
+    }
+    const handleDelete = async (id) => {
+        try {
+            const { data } = await axios.post("http://localhost:8000/book/deletebook", {
+                Id: id,
+            });
+            if (data?.Success) {
+                getAllBookList();
+            }
+        } catch (error) {
+            console.log(error)
+
         }
     }
+    const handleUpdate = async (data) => {
+        setId(data._id)
+        setBookForm({
+            BookName: data.BookName,
+            BookTitle: data.BookTitle,
+            Author: data.Author,
+            SellingPrice: data.SellingPrice,
+            PublishDate: data.PublishDate
+        });
+
+    }
+    useEffect(() => {
+        getAllBookList()
+    }, [])
+
     return (
         <div className='w-full px-5 min-h-[calc(100vh-60px)]'>
             <div className='w-full grid grid-cols-5 gap-3 my-4'>
@@ -97,14 +162,20 @@ function Home() {
                             </tr>
                         </thead>
                         <tbody className='bg-white divide-y divide-gray-200'>
-                            <tr className='hover:bg-gray-200'>
-                                <td className='px-6 py-3 whitespace-nowrap'>NAME</td>
-                                <td className='px-6 py-3 whitespace-nowrap'>NAME</td>
-                                <td className='px-6 py-3 whitespace-nowrap'>NAME</td>
-                                <td className='px-6 py-3 whitespace-nowrap'>NAME</td>
-                                <td className='px-6 py-3 whitespace-nowrap'>NAME</td>
-                                <td className='px-6 py-3 whitespace-nowrap'>NAME</td>
-                            </tr>
+                            {
+                                bookList?.map((book, index) => {
+                                    return (
+                                        <tr key={index} className='hover:bg-gray-200'>
+                                            <td className='px-6 py-3 whitespace-nowrap font-bold text-gray-700 font-serif'>{book?.BookName}</td>
+                                            <td className='px-6 py-3 whitespace-nowrap font-bold text-gray-700 font-serif'>{book?.BookTitle}</td>
+                                            <td className='px-6 py-3 whitespace-nowrap font-bold text-gray-700 font-serif'>{book?.Author}</td>
+                                            <td className='px-6 py-3 whitespace-nowrap font-bold text-gray-700 font-serif'>{book?.SellingPrice}</td>
+                                            <td className='px-6 py-3 whitespace-nowrap font-bold text-gray-700 font-serif'>{book?.PublishDate}</td>
+                                            <td className='px-6 py-3 whitespace-nowrap text-red-700 font-serif'><div className='flex space-x-3'><span onClick={() => handleDelete(book._id)}><MdDelete /></span><span onClick={() => handleUpdate(book)}><FaPen style={{ color: "green" }} /></span></div></td>
+                                        </tr>
+                                    )
+                                })
+                            }
                         </tbody>
                     </table>
                 </div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div className='w-full flex justify-between h-16 items-center bg-gray-200 shadow'>
+        <div className='w-full flex justify-between h-16 items-center bg-gray-700 shadow'>
             <div className='w-[10%] flex items-center h-full ml-8'>
-                <h1 className='font-bold text-zinc-800'>LOGO</h1>
+                <h1 className='font-bold  text-white'>LOGO</h1>
             </div>
             <div className='w-[50%] h-full'>
                 <ul className='w-full h-full flex gap-6 list-none items-center text-zinc-800 font-mediums'>
-                    <li className='cursor-pointer'>HOME</li>
-                    <li className='cursor-pointer'>ABOUT</li>
-                    <li className='cursor-pointer'>CONTACT</li>
+                    <li className='cursor-pointer text-white'>HOME</li>
+                    <li className='cursor-pointer text-white'>ABOUT</li>
+                    <li className='cursor-pointer text-white'>CONTACT</li>
                 </ul>
             </div>
         </div>
